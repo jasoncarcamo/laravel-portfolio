ff2938128a45403f4eeef3bfe2932a6451f2519c
jasoncarcamo <jasoncarcamo30@yahoo.com>
2021-07-28 11:18:34 -0400

    Ready to add migrations to database

---
+++ database/migrations/2021_07_27_012833_projects_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("title");
+            $table->string("description");
+            $table->string("images");
+            $table->string("live_url");
+            $table->string("repo_url");
+            $table->timestamp("date_started");
+            $table->timestamp("date_finished");
+            $table->timestamp("date_created");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}

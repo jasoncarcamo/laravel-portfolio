98576e9adcbd879c82e452e7ab85eb207cf010c3
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2021-08-05 22:47:54 -0400

    Added fake project seeds

--- database/seeders/ProjectsSeeder.php
+++ database/seeders/ProjectsSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProjectsSeeder extends Seeder
 {
@@ -13,6 +14,40 @@ class ProjectsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('projects')->insert([
+            "title" => "jhwebfjhewbhjfq",
+            "description" => "nfwejfew",
+            "images" => "bkwebfef",
+            "live_url" => "nkjknfj2",
+            "repo_url" => "kjbejfq2",
+            "date_started" => date("Y-m-d H:i:s"),
+            "date_finished" => date("Y-m-d H:i:s"),
+            "last_updated" => date("Y-m-d H:i:s"),
+            "date_created" => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('projects')->insert([
+            "title" => "jhweb",
+            "description" => "nfwejfew",
+            "images" => "bkwebfef",
+            "live_url" => "nkjknfj2",
+            "repo_url" => "kjbejfq2",
+            "date_started" => date("Y-m-d H:i:s"),
+            "date_finished" => date("Y-m-d H:i:s"),
+            "last_updated" => date("Y-m-d H:i:s"),
+            "date_created" => date("Y-m-d H:i:s")
+        ]);
+
+        DB::table('projects')->insert([
+            "title" => "jhwebf",
+            "description" => "nfwejfew",
+            "images" => "bkwebfef",
+            "live_url" => "nkjknfj2",
+            "repo_url" => "kjbejfq2",
+            "date_started" => date("Y-m-d H:i:s"),
+            "date_finished" => date("Y-m-d H:i:s"),
+            "last_updated" => date("Y-m-d H:i:s"),
+            "date_created" => date("Y-m-d H:i:s")
+        ]);
     }
 }
